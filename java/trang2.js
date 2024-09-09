6d6f06a83c669fece50d239b16e2c0976b09f40a
Bùi Nguyễn Thúy Ngân <buinguyenthuyngan123@gmail.com>
2024-09-10 02:52:10 +0700

    fad

--- java/trang2.js
+++ java/trang2.js
@@ -1,63 +1,14 @@
-// 
-
 /* Bắt sự kiện click chuột trên ảnh album */
 window.onload = function() {
     let albumImages = document.querySelectorAll('.album');
 
     for(let album of albumImages) {
         album.onclick = function() {
-            let albumName = this.classList[1];
-            window.open(`/trang3.html?album=${albumName}`, '_blank');        
+            let albumName = this.classList[1];  // Lấy tên album từ class thứ 2
+            window.open(`/trang3.html?album=${albumName}`, '_blank');  // Mở trang mới với tên album trong URL
         }
     }
-}
 
-/*window.onload = function() {
-    let search = document.querySelector("input[type=search]");
-    search.onchange = function() {
-        // xóa border
-        let tmps = document.querySelectorAll(".albums > div");
-        for (let t of tmps)
-            t.style.border= "";
-
-        // gắn border product tìm thấy
-        let txt = this.value;
-
-        let products = document.querySelectorAll(".albums p");
-        for (let p of products)
-            if (p.innerText.includes(txt) === true)
-                p.parentElement.style.border = "2px solid red";
-    }
-}*/
-/*
-window.onload = function() {
-
-    let main = document.getElementById("main");
-    let images = document.querySelectorAll(".thumb img");
-    for (let im of images)
-        im.onclick = function() {
-            main.src = this.src;
-        }
-
-    let search = document.querySelector("input[type=search]");
-    search.onchange = function() {
-        // Xóa border của tất cả các albums
-        let tmps = document.querySelectorAll(".albums > div");
-        for (let t of tmps)
-            t.style.border = "";
-
-        // Lấy giá trị từ ô tìm kiếm
-        let txt = this.value;
-
-        // Duyệt qua các sản phẩm và tìm kiếm từ khóa
-        let products = document.querySelectorAll(".albums p");
-        for (let p of products)
-            if (p.innerText.includes(txt) === true)
-                p.parentElement.style.border = "2px solid red";
-    }
-}*/
-
-window.onload = function() {
     let searchInput = document.querySelector("input[type=text]");
     let searchButton = document.querySelector("button");
     
@@ -87,4 +38,35 @@ window.onload = function() {
             alert("Không tìm thấy");
         }
     }
+
+    // Bắt sự kiện click chuột cho mục Yêu thích và Thư viện
+    let yeuthichTab = document.querySelector('a[href="#yeuthich"]');
+    let thuvienTab = document.querySelector('a[href="#thuvien"]');
+
+// Lấy các phần tử chứa nội dung danh sách nhạc
+    let yeuthichSection = document.getElementById('yeuthich');
+    let thuvienSection = document.getElementById('thuvien');
+
+    // Ẩn Thư viện khi vào trang
+    thuvienSection.style.display = 'none';
+
+    // Khi click vào Yêu thích
+    yeuthichTab.onclick = function() {
+        yeuthichSection.style.display = 'block';  // Hiển thị Yêu thích
+        thuvienSection.style.display = 'none';   // Ẩn Thư viện
+    }
+
+    // Khi click vào Thư viện
+    thuvienTab.onclick = function() {
+        thuvienSection.style.display = 'block';  // Hiển thị Thư viện
+        yeuthichSection.style.display = 'none';  // Ẩn Yêu thích
+    }
 }
+
+
+
+
+
+
+
+
