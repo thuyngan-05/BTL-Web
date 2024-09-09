7bce513350fb893d30609536d0ea013a9761ece2
Bùi Nguyễn Thúy Ngân <buinguyenthuyngan123@gmail.com>
2024-09-09 16:07:48 +0700

    xoas ddth

--- java/trang1.js
+++ java/trang1.js
@@ -1,3 +1,17 @@
+/*document.querySelector('.button').addEventListener('click', function () {
+    const username = document.querySelector('.user input').value;
+    const password = document.querySelector('.password input').value;
+
+    // Kiểm tra nếu username và password không rỗng
+    if (username && password) {
+        // Chuyển hướng đến trang chủ của trang web sau khi đăng nhập thành công
+        window.open("/html/trang2.html", "_blank");
+        // window.location.href = '/html/trang2.html'; 
+    } else {
+        alert("Vui lòng nhập đầy đủ username và password.");
+    }
+});*/
+
 // // Bắt sự kiện click chuột trên nút button
 window.onload = function() {
     let button = document.getElementById("button");
